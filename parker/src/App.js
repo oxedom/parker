bbb7e27bece1691e4ccf7e509145b73f8bfb9619
sam <oxedom@gmail.com>
2022-12-24 14:00:33 +0200

    added webcam comp and server comp need to install opencv

---
+++ parker/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Webcam from './comps/Webcam';
+
+function App() {
+  return (
+    <div className="App">
+      <Webcam></Webcam>
+    </div>
+  );
+}
+
+export default App;
