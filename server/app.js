f75f49acb9b65aec93aa5e50dbacf5d1a292831a
sam <oxedom@gmail.com>
2022-12-24 20:52:21 +0200

    updates to server

--- server/app.js
+++ server/app.js
@@ -17,27 +17,6 @@ const cv = require('opencv4nodejs');
 const cors = require('cors');
 
 
-function decodeBase64(base64) {
-  // Create a buffer to hold the binary data
-  const buffer = new Uint8Array(base64.length * 3 / 4);
-
-  // Iterate over the base64 characters
-  for (let i = 0, j = 0; i < base64.length; i += 4) {
-    // Convert each base64 character to a 6-bit binary value
-    const c0 = base64.charCodeAt(i);
-    const c1 = base64.charCodeAt(i + 1);
-    const c2 = base64.charCodeAt(i + 2);
-    const c3 = base64.charCodeAt(i + 3);
-
-    // Convert the 6-bit binary values to 8-bit binary values
-    buffer[j++] = (c0 << 2) | (c1 >> 4);
-    buffer[j++] = ((c1 & 15) << 4) | (c2 >> 2);
-    buffer[j++] = ((c2 & 3) << 6) | c3;
-  }
-
-  // Return the buffer as an ArrayBuffer
-  return buffer.buffer;
-}
 
 
 function decodeBase64ToMat(base64) {
