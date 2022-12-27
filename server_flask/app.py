7dd70119def22527865def81de1ea134647924d2
sam <oxedom@gmail.com>
2022-12-27 19:32:51 +0200

    added gitignore

--- server_flask/app.py
+++ server_flask/app.py
@@ -1,19 +1,29 @@
 from flask import Flask,request
 # from flask_socketio import SocketIO, send
+import os
 import cv2
 import numpy as np
 from flask_cors import CORS
 import json
-import array
+from pathlib import Path
+
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
-# app.config['SECRET_KEY'] = 'secret!'
-# socketio = SocketIO(app, cors_allowed_origins="*")
 
-# @socketio.on('buffer')
-# def handle_message(buffer):
-#       print(buffer)
+haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+app.config['SECRET_KEY'] = 'secret!'
+
+# Root directory of the project
+ROOT_DIR = Path(".")
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Local path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_balloon.h5")
+
 
 
 @app.route('/cv2', methods=['POST'])
@@ -32,22 +42,33 @@ def handle_cv():
 
     arr = np.frombuffer(data, np.uint8)
     img = cv2.imdecode(arr, cv2.IMREAD_COLOR)
-    print(img)
-  
-    return 'PYTHON'
+    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces_rect = haar_cascade.detectMultiScale(
+        gray_image, scaleFactor=1.1, minNeighbors=9)
+    # retval, buffer = cv2.imencode('.jpg', img)
+
+    for (x, y, w, h) in faces_rect:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), thickness=2)
+
+    cv2.imwrite('image.jpg', img)
+
+
+
+
+
+    # print(img)
+    
+    return ''
  
     # Convert the array buffer to a NumPy array
   
-    # print('I got a buffer')
-  
-    # print(buffer)
-    # cv2.imdecode(buffer, cv2.IMREAD_UNCHANGED)
- 
-    # mat = np.asarray(buffer, dtype=np.uint8)     
-    # socketio.emit('gray', cv2.imdecode(arr,'Grayscale'))
+
 
     
 
 if __name__ == '__main__':
     # socketio.run(app)
     app.run()
+
+
