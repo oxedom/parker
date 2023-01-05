989664fbff535b931a49f18a801c9331ac4dd62b
sam <oxedom@gmail.com>
2023-01-05 19:45:20 +0200

    moved things that needed to be in a factory function into a factory function

--- client/src/comps/draw.js
+++ client/src/comps/draw.js
@@ -1,18 +1,148 @@
-let input_imageCapture;
-const canvasWraperRef = document.getElementById("canvasWrapper");
+
 const videoInputRef = document.getElementById("input");
 const overlayRef = document.getElementById("overlay");
 const canvasRef = document.getElementById("canvas");
 const inputCanvasRef = document.getElementById("input-canvas");
 const output = document.getElementById("output");
 
-let sizeSet = false;
-let cam_width = 640;
+
+// get references to the canvas and context
+
+const selectedRegions = [];
+
+
+const renderRectangleFactory = () => 
+{
+
+  const ctx = canvasRef.getContext("2d");
+  const ctxo = overlayRef.getContext("2d");
+  ctx.strokeStyle = "blue";
+  ctx.lineWidth = 10;
+  ctxo.strokeStyle = "blue";
+  ctxo.lineWidth = 4;
+  let canvasOffset = canvasRef.getBoundingClientRect();
+  let offsetX = canvasOffset.left;
+  let offsetY = canvasOffset.top;
+  // this flage is true when the user is dragging the mouse
+  let isDown = false;
+
+  // these vars will hold the starting mouse position
+  let startX;
+  let startY;
+
+  let prevStartX = 0;
+  let prevStartY = 0;
+
+  let prevWidth = 0;
+  let prevHeight = 0;
+
+
+  function handleMouseDown(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  
+    // save the starting x/y of the rectangle
+    startX = parseInt(e.clientX - offsetX);
+    startY = parseInt(e.clientY - offsetY);
+  
+    // set a flag indicating the drag has begun
+    isDown = true;
+  }
+  
+  function handleMouseUp(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  
+    // the drag is over, clear the dragging flag
+    isDown = false;
+  
+    ctxo.strokeRect(prevStartX, prevStartY, prevWidth, prevHeight);
+    addRegionOfIntrest(prevStartX, prevStartY, prevWidth, prevHeight);
+  }
+  
+  function handleMouseOut(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  
+    // the drag is over, clear the dragging flag
+    isDown = false;
+  }
+  
+  function handleMouseMove(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  
+    // if we're not dragging, just return
+    if (!isDown) {
+      return;
+    }
+  
+    // get the current mouse position
+    const mouseX = parseInt(e.clientX - offsetX);
+    const mouseY = parseInt(e.clientY - offsetY);
+  
+    // Put your mousemove stuff here
+  
+    // calculate the rectangle width/height based
+    // on starting vs current mouse position
+    var width = mouseX - startX;
+    var height = mouseY - startY;
+  
+    // clear the canvas
+    ctx.clearRect(0, 0, canvasRef.width, canvasRef.height);
+  
+    // draw a new rect from the start position
+    // to the current mouse position
+    ctx.strokeRect(startX, startY, width, height);
+  
+    prevStartX = startX;
+    prevStartY = startY;
+  
+    prevWidth = width;
+    prevHeight = height;
+  }
+
+
+  
+
+
+  
+
+  
+
+  
+  
+  return {handleMouseDown,handleMouseMove,handleMouseUp,handleMouseOut}
+  }
+
+
+
+
+  function addRegionOfIntrest(prevStartX, prevStartY, prevWidth, prevHeight) {
+    const roiObj = {
+      type: selectedType,
+      cords: {
+        top_x: prevStartX,
+        top_y: prevHeight + prevStartY,
+        bottom_x: prevWidth + prevStartX,
+        bottom_y: prevStartY,
+      },
+    };
+  
+    selectedRegions.push(roiObj);
+  }
+  
+const renderRectangle = renderRectangleFactory()
+
+
+
+// let sizeSet = false;
+// let cam_width = 640;
 let selectedType = "anything";
 const flask_url = "http://127.0.0.1:5000/api/cv/yolo";
 
 //State:
-const selectedRegions = [];
+
 
 const arrOfScreens = [videoInputRef, overlayRef, canvasRef, inputCanvasRef];
 
@@ -46,7 +176,9 @@ function drawCanvas(canvas, img) {
       img.width * ratio,
       img.height * ratio
     );
-}
+
+    }
+
 
 //Sets the size of all the canvas to be the same
 const setSize = (width, height) => {
@@ -54,7 +186,7 @@ const setSize = (width, height) => {
     screen.width = width;
     screen.height = height;
   });
-  sizeSet = true;
+  // sizeSet = true;
 };
 
 const bufferToServer = async (capturedImage) => {
@@ -123,147 +255,30 @@ const getVideo = () => {
 };
 
 getVideo();
-//Get's all the classes
-// fetch('http://127.0.0.1:5000/api/cv/yolo_classes').then((d) =>
-// {
-//     d.json().then(data => { console.log(data);})
 
-// })
 
-// get references to the canvas and context
-const ctx = canvasRef.getContext("2d");
-const ctxo = overlayRef.getContext("2d");
 
-ctx.strokeStyle = "blue";
-ctx.lineWidth = 10;
-ctxo.strokeStyle = "blue";
-ctxo.lineWidth = 4;
 
-let canvasOffset = canvasRef.getBoundingClientRect();
 
-let offsetX = canvasOffset.left;
-let offsetY = canvasOffset.top;
-
-// this flage is true when the user is dragging the mouse
-let isDown = false;
-
-// these vars will hold the starting mouse position
-let startX;
-let startY;
-
-let prevStartX = 0;
-let prevStartY = 0;
-
-let prevWidth = 0;
-let prevHeight = 0;
-
-function handleMouseDown(e) {
-  e.preventDefault();
-  e.stopPropagation();
-
-  // save the starting x/y of the rectangle
-  startX = parseInt(e.clientX - offsetX);
-  startY = parseInt(e.clientY - offsetY);
-
-  // set a flag indicating the drag has begun
-  isDown = true;
-}
-
-function addRegionOfIntrest(prevStartX, prevStartY, prevWidth, prevHeight) {
-  const roiObj = {
-    type: selectedType,
-    cords: {
-      top_x: prevStartX,
-      top_y: prevHeight + prevStartY,
-      bottom_x: prevWidth + prevStartX,
-      bottom_y: prevStartY,
-    },
-  };
-
-  selectedRegions.push(roiObj);
-}
-
-function handleMouseUp(e) {
-  e.preventDefault();
-  e.stopPropagation();
-
-  // the drag is over, clear the dragging flag
-  isDown = false;
-
-  ctxo.strokeRect(prevStartX, prevStartY, prevWidth, prevHeight);
-  addRegionOfIntrest(prevStartX, prevStartY, prevWidth, prevHeight);
-}
-
-function handleMouseOut(e) {
-  e.preventDefault();
-  e.stopPropagation();
-
-  // the drag is over, clear the dragging flag
-  isDown = false;
-}
-
-function handleMouseMove(e) {
-  e.preventDefault();
-  e.stopPropagation();
-
-  // if we're not dragging, just return
-  if (!isDown) {
-    return;
-  }
-
-  // get the current mouse position
-  const mouseX = parseInt(e.clientX - offsetX);
-  const mouseY = parseInt(e.clientY - offsetY);
-
-  // Put your mousemove stuff here
-
-  // calculate the rectangle width/height based
-  // on starting vs current mouse position
-  var width = mouseX - startX;
-  var height = mouseY - startY;
-
-  // clear the canvas
-  ctx.clearRect(0, 0, canvasRef.width, canvasRef.height);
-
-  // draw a new rect from the start position
-  // to the current mouse position
-  ctx.strokeRect(startX, startY, width, height);
-
-  prevStartX = startX;
-  prevStartY = startY;
-
-  prevWidth = width;
-  prevHeight = height;
-}
 
 canvasRef.addEventListener("mousedown", (e) => {
   e.preventDefault();
-  handleMouseDown(e);
+  renderRectangle.handleMouseDown(e);
 });
 
 canvasRef.addEventListener("mousemove", (e) => {
   e.preventDefault();
-  handleMouseMove(e);
+  renderRectangle.handleMouseMove(e);
 });
 
 canvasRef.addEventListener("mouseout", (e) => {
   e.preventDefault();
-  handleMouseOut(e);
+  renderRectangle.handleMouseOut(e);
 });
 
 canvasRef.addEventListener("mouseup", (e) => {
   e.preventDefault();
-  handleMouseUp(e);
+  renderRectangle.handleMouseUp(e);
 });
 
-function checkIfWithin(mother, child) {
-  // Calculate the area of the mother rectangle
 
-  // Calculate the area of the child rectangle
-
-  // // Calculate the overlapping area between the two rectangles
-
-  // Calculate the percentage of the child rectangle that is contained within the mother rectangle
-
-  return 100;
-}
