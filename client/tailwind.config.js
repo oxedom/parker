718314ac43f2deeb2686a63713f361a688692399
sam <oxedom@gmail.com>
2023-01-06 15:31:13 +0200

    added tailwind and postcss

---
+++ client/tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    // For the best performance and to avoid false positives,
+    // be as specific as possible with your content configuration.
+  ],
+};
