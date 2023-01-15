794329fcb5fc9596a0adb89de8ccd4f28a9c30db
sam <oxedom@gmail.com>
2023-01-15 21:32:18 +0200

    Asthetic changes to toolbar

--- client/components/Toolbar.js
+++ client/components/Toolbar.js
@@ -17,12 +17,13 @@ const Toolbar = ({
   return (
     <div className="border-8 p-1  w-80 flex  rounded-l-lg">
       <div className="flex flex-col gap-5">
-        <h2 className="bg-white text-3xl border-black "> Toolbar </h2>
 
-        <div className="flex gap-10 ">
 
-          <div className="flex flex-col items-center">
-          <p> Selecting Color</p>
+      <h2 className="bg-white text-3xl border-b-4 border-black "> Toolbar </h2>
+
+      <div>
+        <div className="border-b-4 border-black ">
+
           <input
             value={selectingBoxColor}
             type="color"
@@ -31,10 +32,7 @@ const Toolbar = ({
               handleColorChange(e, setSelectingBoxColor);
             }}
           />
-          </div>
  
-          <div className="flex flex-col items-center"> 
-          <p> Selected Color</p>
           <input
             className="h-20 w-20"
             value={selectedBoxColor}
@@ -43,37 +41,47 @@ const Toolbar = ({
               handleColorChange(e, setSelectedBoxColor);
             }}
           />
-        </div>
+  
+
+    
 
 
    
 
+
         </div>
+        </div>
+     
+ 
 
-        <div>
-            <select>
-              <option> 1 </option>
-              <option> 2 </option>
-              <option> 2 </option>
-            </select>
 
-            </div>
-
-        <input
+          <div className="flex flex-col gap-5 border-b-4 pb-2 border-black">
+          <input
           onChange={(e) => {
             setRoiName(e.target.value);
           }}
           value={roiName}
           placeholder={"ROI name"}
-          className="border-2 border-black h-8 rounded-md"
+          className="border-2 border-black h-5 rounded-md"
         />
 
-        {/* <select>
-          {classes.map((c) => <option> 1 </option>)}
-        </select> */}
 
+
+          <select className="h-10 w-32"  >
+              <option> Person  </option>
+              <option> Car   </option>
+              <option> Cat </option>
+            </select>
+
+
+          </div>
+
+
+
+
+        <div>
         <div className="flex">
-          <span> FPMS: </span>
+          <label> FPMS: </label>
           <input
             type="number"
             value={fps}
@@ -88,6 +96,8 @@ const Toolbar = ({
         <div className="rounded relative inline-flex group items-center justify-center px-3.5 py-2 m-1 cursor-pointer border-b-4 border-l-2 active:border-blue-600 active:shadow-none shadow-lg bg-gradient-to-tr from-blue-500 to-cyan-400 border-sky-700 text-white">
           {" "}
           <span class="static">Process Video</span>
+        </div>
+
         </div>
       </div>
     </div>
