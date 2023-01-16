9913935a71dfad93d2a97c53f535f16ec78368dd
sam <oxedom@gmail.com>
2023-01-17 00:36:12 +0200

    added draw rect to replace factory function

--- client/components/DrawingCanvas.js
+++ client/components/DrawingCanvas.js
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { renderRectangleFactory } from "../libs/canvas_utility";
 import {imageWidthState, imageHeightState} from '../components/states'
 import { useRecoilState } from "recoil";
-
+import DrawRectangle from "./DrawRectangle";
 const DrawingCanvas = ({children,selectedBoxColor, selectingBoxColor, handleNewRoi, selected}) => {
 
   const [imageWidth] = useRecoilState(imageWidthState)
@@ -14,56 +14,57 @@ const DrawingCanvas = ({children,selectedBoxColor, selectingBoxColor, handleNewR
   const [renderRectangle, setRenderRectangle] = useState(undefined);
 
 
-  useEffect(() => {
-    setRenderRectangle(
-      renderRectangleFactory(drawCanvasRef.current, overlayRef.current)
-    );
-  }, [overlayRef, drawCanvasRef]);
+  // useEffect(() => {
+  //   setRenderRectangle(
+  //     renderRectangleFactory(drawCanvasRef.current, overlayRef.current)
+  //   );
+  // }, [overlayRef, drawCanvasRef]);
 
-  useEffect(() => {
-    if (renderRectangle != undefined) {
-      renderRectangle.setSelectedColor(selectedBoxColor);
-      renderRectangle.setSelectingColor(selectingBoxColor)
-    }
-  }, [renderRectangle, selectedBoxColor, selectingBoxColor]);
+  // useEffect(() => {
+  //   if (renderRectangle != undefined) {
+  //     renderRectangle.setSelectedColor(selectedBoxColor);
+  //     renderRectangle.setSelectingColor(selectingBoxColor)
+  //   }
+  // }, [renderRectangle, selectedBoxColor, selectingBoxColor]);
 
 
 
   return (
-    <>
-      <canvas
-        width={imageWidth}
-        height={imageHeight}
-        ref={drawCanvasRef}
-        className="fixed"
-        style={{ zIndex: 2 }}
-        id="overlay"
-      ></canvas>
+    // <>
+    //   <canvas
+    //     width={imageWidth}
+    //     height={imageHeight}
+    //     ref={drawCanvasRef}
+    //     className="fixed"
+    //     style={{ zIndex: 2 }}
+    //     id="overlay"
+    //   ></canvas>
 
-      <canvas
-        ref={overlayRef}
-        width={imageWidth}
-        height={imageHeight}
-        onMouseDown={(e) => {
-          renderRectangle && renderRectangle.handleMouseDown(e);
-        }}
-        onMouseMove={(e) => {
-          renderRectangle && renderRectangle.handleMouseMove(e);
-        }}
-        onMouseOut={(e) => {
-          renderRectangle && renderRectangle.handleMouseOut(e);
-        }}
-        onMouseUp={(e) => {
-          renderRectangle && renderRectangle.handleMouseUp(e);
+    //   <canvas
+    //     ref={overlayRef}
+    //     width={imageWidth}
+    //     height={imageHeight}
+    //     onMouseDown={(e) => {
+    //       renderRectangle && renderRectangle.handleMouseDown(e);
+    //     }}
+    //     onMouseMove={(e) => {
+    //       renderRectangle && renderRectangle.handleMouseMove(e);
+    //     }}
+    //     onMouseOut={(e) => {
+    //       renderRectangle && renderRectangle.handleMouseOut(e);
+    //     }}
+    //     onMouseUp={(e) => {
+    //       renderRectangle && renderRectangle.handleMouseUp(e);
     
-       }}
-        className="fixed"
-        style={{ zIndex: 2 }}
-        id="draw_canvas"
-      ></canvas>
+    //    }}
+    //     className="fixed"
+    //     style={{ zIndex: 2 }}
+    //     id="draw_canvas"
+    //   ></canvas>
 
-      {children}
-    </>
+    //   {children}
+    // </>
+    <DrawRectangle></DrawRectangle>
   );
 };
 export default DrawingCanvas;
