cf26dc1801af552e0e30db401cd19c74236ee963
sam <oxedom@gmail.com>
2023-03-26 21:12:00 +0300

    added data to view

--- client/pages/view.js
+++ client/pages/view.js
@@ -1,47 +1,34 @@
 
-import { data } from "autoprefixer";
+
 import { useRouter } from "next/router";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const view = () => {
 
     const router = useRouter()
+    const [data, setData] = useState([])
     const peerRef = useRef(null)
     useEffect(() => 
 
-    {
-            
+    { 
     const initPeerJS = async () => {
       
         const { default: Peer } = await import("peerjs");
         const peer = new Peer();
  
-
         peer.on('open', (id) => 
         {
             let conn = peer.connect(router.query.remoteID);
-            console.log(conn)
-            conn.send('a')
 
             conn.on('data', (data) => 
             {
-                console.log(data);
+            console.log(data);
+              setData(data)
+                    
             })
         })
 
-        // peer.on('connection', (conn) => 
-        // {
-        //     conn.on('open', () => {
-        //         console.log('open on view')
-        //     })
 
-        //     conn.on('data', (d) => {
-        //         console.log(data)
-        //     })
-
-        // })
-
-      
         peerRef.current = peer
       };
 
@@ -51,7 +38,7 @@ const view = () => {
 
 
     return ( <div className="h-screen gap-2 pt-10 flex flex-col   min-h-screen  bg-fixed bg-no-repeat bg-cover  bg-filler w-full grow items-center">
-
+        {data.map((el) => { <p> {el.label} </p>})}
     </div> );
 }
  
