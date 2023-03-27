009341842f567dc2b729d13ea565f047bf7db808
sam <oxedom@gmail.com>
2023-03-27 16:33:40 +0300

    about

--- client/pages/about.js
+++ client/pages/about.js
@@ -8,7 +8,7 @@ import Head from "next/head";
 import yoloLogo from "../public/static/yolo.png"
 import tailwindLogo from "../public/static/Tailwind.png"
 import recoilLogo from "../public/static/Recoil.png"
-
+import Accoridan from '../components/Accordion'
 const About = () => {
   return (
     <DefaultLayout>
@@ -70,7 +70,6 @@ const About = () => {
                 
         
 
-         
                 <li className="flex gap-2 text-center items-center">
                   {" "}
                   <Image width={60} src={yoloLogo}></Image>
@@ -111,6 +110,63 @@ const About = () => {
                 </li>
          
               </ul>
+            </section>
+
+            <section className="flex flex-col gap-5">
+              <h3 className="font-bold border-b border-black text-center text-3xl text-gray-800"> Useful information  </h3>
+                
+      
+
+            <div className="mt-1 ">
+            <h4 className="text-2xl font-bold ">
+            Mobile Phone Camera Instructions
+            </h4>
+            <p>
+            After entering the vision page and pressing the remote stream button, you can scan QR code with your mobile device and after the page loads press on the call button to allow acesss to your phones camera, two imporant notes that the phone should be in landscape mode and change your settings that your phone screen doesn't turn off after a mintue. 
+
+            </p>
+
+            </div>
+
+            <div className="flex gap-2 flex-col">
+            <h4 className="text-2xl font-bold   ">
+            Connect CCTV/IP Cameras with OBS 
+            </h4>
+            <p>
+            <strong> If your IP/CCTV cameras not are  not directly connected to your computer </strong>Stream their video footage using
+            <a  className="text-blue-500 " href="https://obsproject.com/"> OBS </a>
+           Window Capture feature and create a virtual webcam on your PC. 
+            </p>
+            <strong>
+             If your IP/CCTV camera is on your local network   </strong> Set your OBS video capture setings to a local ip address.
+            <a  className="text-blue-500" href="https://www.youtube.com/watch?v=0z9Te51rh-4"> Tutorial video</a>
+            <p>
+
+            </p>
+
+            </div>
+
+            <div className="mt-1 ">
+            <h4 className="text-2xl font-bold ">
+            Webcam Instructions
+            </h4>
+            <p>
+            Open the vision page, press the webcam button, allow webcam access, and point it wherever you desire.
+            </p>
+
+            </div>
+
+            <div className="mt-1 ">
+            <h4 className="text-2xl font-bold ">
+            What else can parker detect?
+            </h4>
+            <p>
+              <a href="https://github.com/oxedom/parker/blob/main/client/libs/labels.json" className="text-blue-500"> Here is a list </a>
+         
+            </p>
+
+            </div>
+    
             </section>
           </div>
         </main>
