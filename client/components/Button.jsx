37fa9c067449ebcb48151de89c37b753496feafd
jorismertz <35079666+jorismertz@users.noreply.github.com>
2023-05-28 15:33:37 +0200

    feat: remove tertiary & set default intent

--- client/components/Button.jsx
+++ client/components/Button.jsx
@@ -4,13 +4,15 @@ const styles = cva("py-1.5 px-2 font-medium rounded-md transition-colors", {
   variants: {
     intent: {
       primary: "bg-blue-500 hover:bg-blue-400 text-white",
-      secondary: "bg-gray-300 text-gray-700",
-      tertiary: "bg-white hover:bg-gray-200 text-slate-800",
+      secondary: "bg-white hover:bg-gray-200 text-slate-800",
       destructive: "bg-red-500 hover:bg-red-400 text-white",
     },
     fullWidth: {
       true: "w-full",
     },
+  },
+  defaultVariants: {
+    intent: "secondary",
   },
 });
 
@@ -21,7 +23,7 @@ const styles = cva("py-1.5 px-2 font-medium rounded-md transition-colors", {
  *  Click me!
  * </Button>
  * ```
- * @param intent - primary, secondary, tertiary, destructive (determines color of button)
+ * @param intent - primary, secondary, destructive (determines color of button)
  * @param fullWidth - true, false (self explanatory)
  */
 export default function Button({ children, ...buttonProps }) {
