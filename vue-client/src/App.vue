bcb265f9740bb16a43722f2986f297f490ab593d
Sam <oxedom@gmail.com>
2024-10-07 12:29:55 +0300

    fix: loading modal works

--- vue-client/src/App.vue
+++ vue-client/src/App.vue
@@ -1,9 +1,23 @@
-<script setup lang="ts">
-import './index.css'
-</script>
-
 <template>
   <RouterView />
+
+  <button @click="runYolo">Run Yolo</button>
 </template>
+
+<script setup lang="ts">
+import './index.css'
+import * as tf from '@tensorflow/tfjs'
+
+const runYolo = async () => {
+  let yolov7 = await tf.loadGraphModel(`${window.location.origin}/yolov7_web_model/model.json`, {
+    onProgress: (fractions) => {
+      //Loading
+      console.log(`Yolo7 model loaded: ${fractions}%`)
+    }
+  })
+
+  console.log('Yolo7 model loaded', yolov7)
+}
+</script>
 
 <style scoped></style>
